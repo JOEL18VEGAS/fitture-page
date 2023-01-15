62faf28e8eb4d199ed1042b565dcaaf18a9094fb
JoelJvegas <joel18gonzalez@outlook.com>
2023-01-14 19:05:39 -0500

    Initial Commit

---
+++ fitture/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Home from './components/Home.vue'
+import 'boxicons'
+</script>
+
+<template>
+  <header>
+    <nav class="flex  justify-between px-32 py-10">
+      <div style="@import url('https://fonts.googleapis.com/css2?family=Courgette&display=swap'); font-family: 'Courgette', cursive;" class="flex items-center">
+        <img class="h-16" src="./assets/img/musclewhite.svg" alt="logo">
+        <h4 class="text-2xl">Fitture</h4>
+      </div>
+      <div class="flex items-center pr-10">
+        <ul style="@import url('https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@200&display=swap'); font-family: 'Sofia Sans', sans-serif;" class="flex justify-center text-lg space-x-28">
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Pricing</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+          <button style="clip-path: polygon(0 0, 100% 0, 100% 62%, 85% 100%, 0 100%);" class="bg-white px-7 py-4  ml-20 text-black font-bold ">Register Now</button>
+      </div>
+    </nav>
+  </header>
+  <Home />
+</template>
+
---
+++ fitture/src/components/Home.vue
@@ -0,0 +1,50 @@
+<script setup>
+
+import 'boxicons'
+</script>
+
+<template>
+  <main>
+    <section>
+      <div class="flex justify-between">
+        <div class="pl-32 pt-32 lg:w-1/2">
+          <h2 id="title-mk" class=" text-8xl">MAKE YOUR</h2>
+          <h1 id="title-bd" class="py-10 text-9xl font-bold ">BODY SHAPE</h1>
+          <p id="p-title" class="text-xl text-slate-200 w-4/6 mb-20">In regard to using gym work experience to help with my college course I was able to do soon.</p>
+          <div>
+            <button id="principal-btn" class="flex items-center bg-lime-500 px-12 py-6 text-xl text-black font-bold ">Get Started <span class="w-5"></span> <box-icon name='right-arrow-alt'></box-icon></button>
+          </div>
+        </div>
+        <div id="bg-container w-1/2">
+          <!-- <img class="h-1/2" src="../assets/img/person.png" alt="bg"> -->
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css2?family=Londrina+Sketch&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Sofia+Sans:wght@900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap');
+
+#title-mk {
+  font-family: 'Londrina Sketch', cursive;
+  color: #fff;
+}
+
+#title-bd {
+  font-family: 'Sofia Sans', sans-serif;
+  color: #fff;
+}
+
+#p-title {
+  font-family: 'Nunito', sans-serif;
+}
+
+#principal-btn {
+
+  clip-path: polygon(0 0, 100% 0, 100% 62%, 88% 100%, 0 100%);
+}
+</style>
